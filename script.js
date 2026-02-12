938e3763dd8426a3af93601ad118be11d1ed2495
Xavivo <albertovazquezmonge@gmail.com>
2026-02-12 09:39:50 +0100

    const called and is receiving data form API

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+// Variable global para guardar los datos que nos dé la API
+let usuariosGlobal = [];
+// Función para hacer la petición a la API y guardar los datos en la variable global
 const fetchJSON = async () => {
   fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+      .then(response => {
+      // Si la respuesta no es ok, error
+      if (!response.ok) throw Error("No se pudo obtener la información");
+      // Al contrario, devuelve JSON
+      return response.json();
+      })
+      .then(json => {
+        console.log("json cargado: ", json);
+      })
 }
