101a855b43ad7f2c04b269b4b9547570083b779a
Xavivo <albertovazquezmonge@gmail.com>
2026-02-12 10:31:55 +0100

    table created

--- script.js
+++ script.js
@@ -11,26 +11,25 @@ const fetchJSON = async () => {
       })
       .then(json => {
         datosJSON = json;
-        console.log("json cargado: ", json);
         resultados(datosJSON);
       })
 }
 
 function resultados(datos) {
-  const resultadosDiv = document.getElementById("resultado");
+  const resultadosDiv = document.getElementById("body");
   resultadosDiv.innerHTML = "";
   datos.forEach(usuario => {
-    const usuarioDiv = document.createElement("div");
+    const usuarioDiv = document.createElement("tr");
     usuarioDiv.innerHTML = `
-      <h3>${usuario.username}</h3>
-      <p>ID: ${usuario.id}</p>
-      <p>Email: ${usuario.email}</p>
-      <p>Ciudad: ${usuario.address.city}</p>
-      <p>Calle: ${usuario.address.street}</p>
-      <p>Suite: ${usuario.address.suite}</p>
-      <p>Zip Code: ${usuario.address.zipcode}</p>
-      <p>geo: lat ${usuario.address.geo.lat}</p>
-      <p>geo: lng ${usuario.address.geo.lng}</p>
+      <td>${usuario.username}</td>
+      <td>${usuario.id}</td>
+      <td>${usuario.email}</td>
+      <td>${usuario.address.city}</td>
+      <td>${usuario.address.street}</td>
+      <td>${usuario.address.suite}</td>
+      <td>${usuario.address.zipcode}</td>
+      <td>${usuario.address.geo.lat}</td>
+      <td>${usuario.address.geo.lng}</td>
     `;
     resultadosDiv.appendChild(usuarioDiv);
   });
